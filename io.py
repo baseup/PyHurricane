84b26d1b7ad930d8e205eb15e2c50d8dc119cae1
Anthony De Leon <tonton.dev001@gmail.com>
2015-03-01 18:19:32 +0800

    update async_task method

--- io.py
+++ io.py
@@ -1,9 +1,11 @@
 from concurrent.futures import ThreadPoolExecutor
+from tornado import gen
 
 class AsyncTaskPool:
 
     def __init__(self):
         self._pool = ThreadPoolExecutor(max_workers=None)
 
-    def async_task(self):
-        return self._pool.submit()
+    @gen.coroutine
+    def async_task(self, blocking_func, args=None):
+        return yield self._pool.submit(blocking_func, args)
